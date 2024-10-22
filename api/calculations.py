d45bc7d04686f326d21c328a2a2cd82f74ba2d9b
Raihanul Haque Rahi <raihanulhaque007@gmail.com>
2024-10-22 23:01:13 +0600

    Add divide function and tests for calculations module

--- api/calculations.py
+++ api/calculations.py
@@ -9,5 +9,8 @@ def subtract(a, b):
 def multiply(a, b):
     return a * b
 
+def divide(a, b):
+    return (a / b)
+
 
 print(add(1, 2))  # 3
--- test_calculations.py
+++ test_calculations.py
@@ -1,7 +1,12 @@
-from api.calculations import add, multiply
+from api.calculations import add, multiply, divide
 
 def test_add():
     assert add(1, 2) == 3
 
 def test_multiply():
     assert multiply(2, 3) == 6
+
+def test_divide():
+    assert divide(6, 3) == 2
+    assert divide(5, 2) == 2.5
+    assert divide(5, 0) == "You can't divide by zero!"
